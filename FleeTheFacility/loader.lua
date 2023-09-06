26b52599c118e0f51471ae44d930b61787b7a365
InfinityMercury <141868217+InfinityMercury@users.noreply.github.com>
2023-09-05 21:30:12 -0300

    Update loader.lua

--- FleeTheFacility/loader.lua
+++ FleeTheFacility/loader.lua
@@ -1,4 +1,4 @@
-__VERSION = '1.8'
+__VERSION__ = '2.0'
 
 
 
@@ -63,6 +63,17 @@ local function WalkSpeedBypass()
     return oldIndex(self, b)
   end)
 end
+local function JumpPowerBypass()
+  local gmt = getrawmetatable(game)
+  setreadonly(gmt, false)
+  local oldIndex = gmt.__Index
+  gmt.__Index = newcclosure(function(self, b)
+    if b == 'JumpPower' then
+      return 50
+    end
+    return oldIndex(self, b)
+  end)
+end
 local BeastColor = Color3.new(255, 0, 0)
 local InoccentColor = Color3.new(255, 255, 255)
 local CreateEsp = function()
@@ -95,154 +106,136 @@ local hrp = chr.HumanoidRootPart
 
 
 
--- Libray settings
-getgenv().SecureMode = true
-local ArrayField = loadstring(game:HttpGet("https://raw.githubusercontent.com/InfinityMercury/Scripts/main/Ui%20Libray/Arrayfield.lua",true))()
-local Window = ArrayField:CreateWindow({
-   Name = "Infinity Hub | "..game:GetService('MarketplaceService'):GetProductInfo(game.PlaceId).Name,
-   LoadingTitle = "Infinity Hub",
-   LoadingSubtitle = "by infinity mercury",
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil,
-      FileName = "__InfinityHub"
-   },
-   Discord = {
-      Enabled = true,
-      Invite = "no invite link",
-      RememberJoins = true
-   },
-   KeySystem = false,
-   KeySettings = {
-      Title = "Infinity Hub",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided",
-      FileName = "Key",
-      SaveKey = true,
-      GrabKeyFromSite = true,
-      Actions = {
-            [1] = {
-                Text = 'Click here to copy key',
-                OnPress = function()
-                    setclipboard([[Abkdaisjd(&!428345968792j]])
-                end,
-                }
-            },
-      Key = {"Abkdaisjd(&!428345968792j"}
-   }
+-- Libray
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+local Window = Fluent:CreateWindow({
+    Title = 'Infinity Hub | '..__VERSION__..' | '..game:GetService('MarketplaceService'):GetProductInfo(game.PlaceId).Name,
+    SubTitle = "by InfinityMercury",
+    TabWidth = 100,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = false,
+    Theme = "Darker",
+    MinimizeKey = Enum.KeyCode.J
 })
-wait(.5)
-for _, v in pairs(game:GetService('CoreGui'):GetDescendants()) do
-	if v:IsA('ScreenGui') and v.Name == 'ArrayField' then
-		v.Main.Topbar.Type.ImageTransparency = 1 wait(.1)
-        v.Main.Topbar.BackgroundColor3 = Color3.new(0.058823, 0.007843, 0.494117) wait(.1) v.Main.Topbar.CornerRepair.BackgroundColor3 = Color3.new(0.058823, 0.007843, 0.494117) wait(.1) v.Main.Topbar.Divider.BackgroundColor3 = Color3.new(0.007843, 0.333333, 0.6) wait(.25) 
-        v.Main.Topbar.Type.Image = 'http://www.roblox.com/asset/?id=7733993211'
-        v.Main.Topbar.Type.ImageTransparency = 0
-	end
-end
-
-
--- code
-local WelcomeTab = Window:CreateTab("// Welcome //", 14203785111)
-local WelcomeSection = WelcomeTab:CreateSection("// Welcome To Infinity Hub //", false)
-local Label = WelcomeTab:CreateLabel("Welcome to Inifnity Hub, "..game.Players.LocalPlayer.Name)
-local Label = WelcomeTab:CreateLabel("Hub version: ".. __VERSION)
-local Label = WelcomeTab:CreateLabel("Made by InfinityMercury")
+local Tabs = {
+    Farming = Window:AddTab({ Title = "Farming", Icon = "rbxassetid://7743866529" }),
+    Esp = Window:AddTab({ Title = "Esp", Icon = "rbxassetid://7733774602" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+local Options = Fluent.Options
 
 
 
-
-
-local FarmingTab = Window:CreateTab("// Farming //", 13501204139)
-local PlayerSection = FarmingTab:CreateSection("// Player //", false)
-local Toggle = FarmingTab:CreateToggle({
-   Name = "Nefer fall hacking",
-   CurrentValue = false,
-   Flag = "Toggle1",
-   Callback = function(state)
-       NeverFallHack = state
-       if NeverFallHack then
-           while wait() and NeverFallHack do
-                game:GetService("ReplicatedStorage").RemoteEvent:FireServer(
-                    "SetPlayerMinigameResult",
-                    true
-                )
-           end
-       end
-   end,
-})
-local Toggle = FarmingTab:CreateToggle({
-   Name = "No slow",
-   CurrentValue = false,
-   Flag = "Toggle1",
-   Callback = function(state)
-       NoSlow = state
-       if NoSlow then
-           WalkSpeedBypass()
-           while wait(.5) and NoSlow do
-               if game.Players.LocalPlayer.Character.Humanoid.WalkSpeed < 16 then
-                    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
-               end
-           end
-       end
-   end,
-})
-
-
-local PlayerSection = FarmingTab:CreateSection("// Esp //", false)
-local Toggle = FarmingTab:CreateToggle({
-   Name = "Player Esp",
-   CurrentValue = false,
-   Flag = "Toggle1",
-   Callback = function(state)
-       PlayerEsp = state
-       if PlayerEsp then
-          CreateEsp()
-          while wait() and PlayerEsp do
-            UpdateEsp()
+-- Code
+local AutoHackToggle = Tabs.Farming:AddToggle("MyToggle", {Title = "Auto Hack", Default = false })
+AutoHackToggle:OnChanged(function(bool)
+    AutoHack = bool
+    while AutoHack do task.wait()
+        game:GetService("ReplicatedStorage").RemoteEvent:FireServer("SetPlayerMinigameResult",true)
+    end
+end)
+local NoSlowToggle = Tabs.Farming:AddToggle("MyToggle", {Title = "No Slow", Default = false })
+NoSlowToggle:OnChanged(function(bool)
+    NoSlow = bool
+    if NoSlow then
+        WalkSpeedBypass()
+        while NoSlow do task.wait(.4)
+            if game.Players.LocalPlayer.Character.Humanoid.WalkSpeed < 16 then
+                game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+            end
+        end
+    end
+end)
+Tabs.Farming:AddButton({
+    Title = "Check Beast",
+    Description = "Click to check the beast",
+    Callback = function()
+        for _, v in pairs(game:GetService('Players'):GetChildren()) do
+          if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+            if v.Character:findFirstChild('BeastPowers') then
+              Fluent:Notify({
+                  Title = "Beast Check",
+                  Content = "Beast is: "..v.Name,
+                  Duration = 8
+              })
+            end
           end
-       else
-         for _, x in pairs(workspace:GetDescendants()) do
-           if x:IsA('Highlight') and x.Name == 'EspPlayer' then
-             x:Destroy()
-           end
+        end
+    end
+})
+local Slider = Tabs.Farming:AddSlider("Slider", {
+    Title = "WalkSpeed",
+    Description = "WalkSpeed slider",
+    Default = 16,
+    Min = 16,
+    Max = 500,
+    Rounding = 0,
+    Callback = function(Value)
+        WalkSpeedBypass()
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+    end
+})
+local Slider = Tabs.Farming:AddSlider("Slider", {
+    Title = "JumpPower",
+    Description = "JumpPower slider",
+    Default = 50,
+    Min = 50,
+    Max = 500,
+    Rounding = 0,
+    Callback = function(Value)
+        JumpPowerBypass()
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+    end
+})
+
+
+local PlayerEspToggle = Tabs.Esp:AddToggle("MyToggle", {Title = "Players Esp", Default = false })
+PlayerEspToggle:OnChanged(function(bool)
+    PlayersEsp = bool
+    if PlayersEsp then
+        CreateEsp()
+        while PlayersEsp do task.wait()
+          UpdateEsp()
+        end
+     else
+       for _, x in pairs(workspace:GetDescendants()) do
+         if x:IsA('Highlight') and x.Name == 'EspPlayer' then
+           x:Destroy()
          end
        end
-   end,
-})
-local Toggle = FarmingTab:CreateToggle({
-   Name = "Computer Esp",
-   CurrentValue = false,
-   Flag = "Toggle1",
-   Callback = function(state)
-       ComputerEsp = state
-       if ComputerEsp then
-            local map = workspace:findFirstChild(tostring(game.ReplicatedStorage.CurrentMap.Value))
-            local children = map:GetChildren()
-                for i =1, #children do
-                if children[i].Name == "ComputerTable" then
-                    local Box = Instance.new("BoxHandleAdornment")
-                    Box.Size = GetSizeOfObject(children[i].Screen) + Vector3.new(-0.5, -0.5, -0.5)
-                    Box.Name = "ESPPart"
-                    Box.Adornee = children[i].Screen
-                    spawn (function()
-                        while true do
-                            wait(0.1)
-                            Box.Color3 = children[i].Screen.Color
-                        end
-                    end)
-                    Box.AlwaysOnTop = true
-                    Box.ZIndex = 5
-                    Box.Transparency = 0.4
-                    Box.Parent = ESPPC
-                end
+     end
+end)
+local ComputersEspToggle = Tabs.Esp:AddToggle("MyToggle", {Title = "Computers Esp", Default = false })
+ComputersEspToggle:OnChanged(function(bool)
+    CumputersEsp = bool
+    if CumputersEsp then
+        local map = workspace:findFirstChild(tostring(game.ReplicatedStorage.CurrentMap.Value))
+        local children = map:GetChildren()
+            for i =1, #children do
+            if children[i].Name == "ComputerTable" then
+                local Box = Instance.new("BoxHandleAdornment")
+                Box.Size = GetSizeOfObject(children[i].Screen) + Vector3.new(-0.5, -0.5, -0.5)
+                Box.Name = "ESPPart"
+                Box.Adornee = children[i].Screen
+                spawn (function()
+                    while true do
+                        wait(0.1)
+                        Box.Color3 = children[i].Screen.Color
+                    end
+                end)
+                Box.AlwaysOnTop = true
+                Box.ZIndex = 5
+                Box.Transparency = 0.4
+                Box.Parent = ESPPC
             end
-        else
-            for _, v in pairs(workspace.ESPComputer:GetChildren()) do
-                if v:IsA('BoxHandleAdornment') then
-                    v:Destroy()
-                end
+        end
+    else
+        for _, v in pairs(workspace:GetDescendants()) do
+            if v:IsA('BoxHandleAdornment') then
+                v:Destroy()
             end
-       end
-   end,
-})
+        end
+   end
+end)
